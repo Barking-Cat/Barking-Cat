047f31128262b2c2af6b91d2a3ce2ae05897ee7b
82109 <qwe199942@gmail.com>
2021-09-11 21:05:06 +0900

    First Commit

---
+++ BarkingCat/src/main/java/PetShop/BarkingCat/BarkingCatApplication.java
@@ -0,0 +1,13 @@
+package PetShop.BarkingCat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarkingCatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarkingCatApplication.class, args);
+	}
+
+}
---
+++ BarkingCat/src/test/java/PetShop/BarkingCat/BarkingCatApplicationTests.java
@@ -0,0 +1,13 @@
+package PetShop.BarkingCat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarkingCatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
