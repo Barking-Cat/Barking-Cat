c12be3dff0cb5ef50cbc8b046e65f947fc36a4bc
82109 <qwe199942@gmail.com>
2021-09-30 01:43:26 +0900

    feat: join member 기능 추가

---
+++ BarkingCat/src/main/java/PetShop/BarkingCat/domain/member/MemberController.java
@@ -0,0 +1,30 @@
+package PetShop.BarkingCat.domain.member;
+
+import PetShop.BarkingCat.domain.member.dto.MemberForm;
+import PetShop.BarkingCat.domain.member.service.MemberService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/member")
+public class MemberController {
+
+    private final MemberService memberService;
+
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> joinMember(@RequestBody MemberForm memberForm){
+
+        memberService.joinMember(memberForm);
+
+        return ResponseEntity.ok(HttpStatus.CREATED);
+
+    }
+}
