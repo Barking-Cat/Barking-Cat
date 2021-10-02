ceb87c06141f0ab4b34da7e11d1dd7363df49e7f
Seonho Cho <jshone144@gmail.com>
2021-10-02 10:17:22 +0900

    Validate signup form

---
+++ front/pages/signup/index.tsx
@@ -0,0 +1,87 @@
+import type { NextPage } from 'next';
+import React from 'react';
+import styles from './signup.module.css';
+import { SubmitHandler, useForm } from 'react-hook-form';
+
+const EMAIL_PATTERN =
+  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+interface SignUpInput {
+  name: string;
+  email: string;
+  password: string;
+  passwordRepeat: string;
+  phone: string;
+}
+
+const SignUp: NextPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    getValues,
+  } = useForm<SignUpInput>({ mode: 'all' });
+
+  const onSubmit: SubmitHandler<SignUpInput> = (data) => console.log({ data });
+
+  return (
+    <div>
+      <h1>회원가입</h1>
+      <button>카카오로 계속하기</button>
+      <button>Facebook으로 계속하기</button>
+      <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+        <input
+          aria-label="name"
+          type="username"
+          placeholder="이름"
+          {...register('name', { required: '이름을 입력해 주세요.' })}
+        />
+        {errors.name && <p>{errors.name.message}</p>}
+        <input
+          aria-label="email"
+          type="email"
+          autoComplete="username"
+          placeholder="이메일"
+          {...register('email', {
+            required: '이메일을 입력해 주세요.',
+            pattern: {
+              value: EMAIL_PATTERN,
+              message: '유효하지 않은 이메일입니다.',
+            },
+          })}
+        />
+        {errors.email && <p>{errors.email.message}</p>}
+        <input
+          aria-label="password"
+          type="password"
+          autoComplete="new-password"
+          placeholder="비밀번호"
+          {...register('password', { required: '비밀번호를 입력해 주세요.' })}
+        />
+        {errors.password && <p>{errors.password.message}</p>}
+        <input
+          aria-label="password-repeat"
+          type="password"
+          autoComplete="new-password"
+          placeholder="비밀번호 확인"
+          {...register('passwordRepeat', {
+            required: '비밀번호를 다시 입력해 주세요.',
+            validate: (value) =>
+              value === getValues().password || '비밀번호가 일치하지 않습니다.',
+          })}
+        />
+        {errors.passwordRepeat && <p>{errors.passwordRepeat.message}</p>}
+        <input
+          aria-label="phone"
+          type="text"
+          placeholder="핸드폰 번호"
+          {...register('phone', { required: '핸드폰 번호를 입력해 주세요.' })}
+        />
+        {errors.phone && <p>{errors.phone.message}</p>}
+        <button>계정 만들기</button>
+      </form>
+    </div>
+  );
+};
+
+export default SignUp;
---
+++ front/pages/signup/signup.test.tsx
@@ -0,0 +1,131 @@
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import SignUp from '.';
+
+describe('Signup Page', () => {
+  it('should render signup header', () => {
+    render(<SignUp />);
+
+    const headerEl = screen.getByRole('heading', { name: /회원가입/g });
+    expect(headerEl).toBeInTheDocument();
+  });
+
+  it('should render kakao button', () => {
+    render(<SignUp />);
+
+    const buttonEl = screen.getByRole('button', { name: /카카오로 계속하기/g });
+    expect(buttonEl).toBeInTheDocument();
+  });
+
+  it('should render facebook button', () => {
+    render(<SignUp />);
+
+    const buttonEl = screen.getByRole('button', {
+      name: /Facebook으로 계속하기/g,
+    });
+    expect(buttonEl).toBeInTheDocument();
+  });
+
+  it('should render name input field', () => {
+    render(<SignUp />);
+
+    const nameInputEl = screen.getByRole('textbox', { name: /name/i });
+    expect(nameInputEl).toBeInTheDocument();
+  });
+
+  it('should render email input field', () => {
+    render(<SignUp />);
+
+    const emailInputEl = screen.getByRole('textbox', { name: /email/i });
+    expect(emailInputEl).toBeInTheDocument();
+  });
+
+  it('should render password input field', () => {
+    render(<SignUp />);
+
+    const passwordInputEl = screen.getByPlaceholderText('비밀번호');
+    expect(passwordInputEl).toBeInTheDocument();
+  });
+
+  it('should render password confirm input field', () => {
+    render(<SignUp />);
+
+    const passwordRepeatInputEl = screen.getByPlaceholderText('비밀번호 확인');
+    expect(passwordRepeatInputEl).toBeInTheDocument();
+  });
+
+  it('should render phone number input field', () => {
+    render(<SignUp />);
+
+    const mobileNumberInputEl = screen.getByRole('textbox', {
+      name: /phone/i,
+    });
+    expect(mobileNumberInputEl).toBeInTheDocument();
+  });
+
+  it('should render submit button', () => {
+    render(<SignUp />);
+
+    const buttonEl = screen.getByRole('button', {
+      name: /계정 만들기/g,
+    });
+    expect(buttonEl).toBeInTheDocument();
+  });
+});
+
+describe('Email validation', () => {
+  it('should render error message when email is invalid', async () => {
+    render(<SignUp />);
+
+    userEvent.type(screen.getByRole('textbox', { name: /email/i }), 'abc.com');
+    await waitFor(() =>
+      expect(
+        screen.queryByText('유효하지 않은 이메일입니다.')
+      ).toBeInTheDocument()
+    );
+  });
+
+  it('should not render error message when email is valid', async () => {
+    render(<SignUp />);
+
+    userEvent.type(
+      screen.getByRole('textbox', { name: /email/i }),
+      'test@test.com'
+    );
+    await waitFor(() =>
+      expect(
+        screen.queryByText('유효하지 않은 이메일입니다.')
+      ).not.toBeInTheDocument()
+    );
+  });
+});
+
+describe('Password validation', () => {
+  it('should warn when passwords are not the same', async () => {
+    render(<SignUp />);
+
+    userEvent.type(screen.getByPlaceholderText('비밀번호'), 'pass');
+
+    userEvent.type(screen.getByPlaceholderText('비밀번호 확인'), 'passpass');
+
+    await waitFor(() => {
+      expect(
+        screen.queryByText('비밀번호가 일치하지 않습니다.')
+      ).toBeInTheDocument();
+    });
+  });
+
+  it('should not warn when passwords are the same', async () => {
+    render(<SignUp />);
+
+    userEvent.type(screen.getByPlaceholderText('비밀번호'), 'pass');
+
+    userEvent.type(screen.getByPlaceholderText('비밀번호 확인'), 'pass');
+
+    await waitFor(() => {
+      expect(
+        screen.queryByText('비밀번호가 일치하지 않습니다.')
+      ).not.toBeInTheDocument();
+    });
+  });
+});
