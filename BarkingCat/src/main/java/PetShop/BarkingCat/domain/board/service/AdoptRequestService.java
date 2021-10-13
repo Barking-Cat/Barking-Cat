099206409b6ceb8336555073b327c742bf9ebc2c
강민형 <56009774+rkdals213@users.noreply.github.com>
2021-10-13 22:32:51 +0900

    feat: 입양신청서 상태 변경 로직 구현

--- BarkingCat/src/main/java/PetShop/BarkingCat/domain/board/service/AdoptRequestService.java
+++ BarkingCat/src/main/java/PetShop/BarkingCat/domain/board/service/AdoptRequestService.java
@@ -5,24 +5,30 @@ import PetShop.BarkingCat.domain.board.model.AdoptRequest;
 import PetShop.BarkingCat.domain.board.model.Board;
 import PetShop.BarkingCat.domain.board.repository.AdoptRequestRepository;
 import PetShop.BarkingCat.domain.board.repository.BoardRepository;
+import PetShop.BarkingCat.domain.board.repository.query.AdoptRequestQueryRepository;
 import PetShop.BarkingCat.domain.member.repository.MemberRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class AdoptRequestService {
 
     private final AdoptRequestRepository adoptRequestRepository;
 
+    private final AdoptRequestQueryRepository adoptRequestQueryRepository;
+
     private final BoardRepository boardRepository;
 
     private final MemberRepository memberRepository;
 
-    public AdoptRequestService(AdoptRequestRepository adoptRequestRepository, BoardRepository boardRepository, MemberRepository memberRepository) {
+    public AdoptRequestService(AdoptRequestRepository adoptRequestRepository, AdoptRequestQueryRepository adoptRequestQueryRepository, BoardRepository boardRepository, MemberRepository memberRepository) {
         this.adoptRequestRepository = adoptRequestRepository;
+        this.adoptRequestQueryRepository = adoptRequestQueryRepository;
         this.boardRepository = boardRepository;
         this.memberRepository = memberRepository;
     }
 
+    @Transactional
     public Long registerAdoptRequest(Long memberId, AdoptRequestForm adoptRequestForm) {
         Board board = boardRepository.findById(adoptRequestForm.getBoardId())
                 .orElseThrow(() -> new RuntimeException("존재하지 않는 게시물입니다"));
@@ -36,5 +42,29 @@ public class AdoptRequestService {
 
         return adoptRequestRepository.save(adoptRequest)
                 .id();
+    }
+
+    @Transactional
+    public void progressAdoptRequest(Long memberId, Long adoptRequestId) {
+        AdoptRequest adoptRequest = adoptRequestRepository.findById(adoptRequestId)
+                .orElseThrow(() -> new RuntimeException("존재하지 않은 요청입니다"));
+
+        if (adoptRequest.boardWriterIsNotEqual(memberId)) {
+            throw new RuntimeException("본인의 게시물의 요청만 진행할 수 있습니다");
+        }
+
+        adoptRequest.progress();
+    }
+
+    @Transactional
+    public void cancelAdoptRequest(Long memberId, Long adoptRequestId) {
+        AdoptRequest adoptRequest = adoptRequestRepository.findById(adoptRequestId)
+                .orElseThrow(() -> new RuntimeException("존재하지 않은 요청입니다"));
+
+        if (adoptRequest.writerIsNotEqual(memberId)) {
+            throw new RuntimeException("본인의 요청이 아니면 취소할 수 없습니다");
+        }
+
+        adoptRequest.cancel();
     }
 }
