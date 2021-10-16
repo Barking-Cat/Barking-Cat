982927ddd57dacdccfbbf2ca43cc7d7650f87b46
hyjoong <maplesosser@naver.com>
2021-10-16 21:00:37 +0900

    Add Adoption page

--- front/pages/adoption/index.tsx
+++ front/pages/adoption/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { NextPage } from 'next';
 
 const Adoption: NextPage = () => {
-  return <div>adoption page</div>;
+  return <div>adoption pages</div>;
 };
 
 export default Adoption;
