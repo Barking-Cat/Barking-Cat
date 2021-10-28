1bc679e2c3793af38566d61d52d5e4706d35d253
82109 <qwe199942@gmail.com>
2021-10-28 20:34:58 +0900

    feat: MyPage 내 정보 조회 API 추가

--- BarkingCat/src/main/java/PetShop/BarkingCat/domain/member/MemberController.java
+++ BarkingCat/src/main/java/PetShop/BarkingCat/domain/member/MemberController.java
@@ -1,6 +1,10 @@
 package PetShop.BarkingCat.domain.member;
 
+import PetShop.BarkingCat.common.security.annotations.JwtClaim;
 import PetShop.BarkingCat.domain.member.service.MemberService;
+import PetShop.BarkingCat.domain.member.service.query.MemberQueryService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,9 +13,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberController {
 
     private final MemberService memberService;
+    private final MemberQueryService memberQueryService;
 
-    public MemberController(MemberService memberService) {
+    public MemberController(MemberService memberService, MemberQueryService memberQueryService) {
         this.memberService = memberService;
+        this.memberQueryService = memberQueryService;
     }
+
+    @GetMapping("/mypage")
+    public ResponseEntity<?> findMyPage(@JwtClaim("info.id") Long memberId){
+
+        return ResponseEntity.ok(memberQueryService.findMyPage(memberId));
+
+    }
+
 
 }
