c94670d4a7ed23a07c9b9a598d550c72fc703e7a
82109 <qwe199942@gmail.com>
2021-11-17 20:46:58 +0900

    feat: 댓글조회 api response에 comment_id 값 추가

--- BarkingCat/src/main/java/PetShop/BarkingCat/domain/board/dto/CommentResponse.java
+++ BarkingCat/src/main/java/PetShop/BarkingCat/domain/board/dto/CommentResponse.java
@@ -8,6 +8,8 @@ import java.time.LocalDateTime;
 @Getter
 public class CommentResponse {
 
+    private final Long commentId;
+
     private final String content;
 
     private final String name;
@@ -15,7 +17,8 @@ public class CommentResponse {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private final LocalDateTime createdDate;
 
-    public CommentResponse(String content, String name, LocalDateTime createdDate) {
+    public CommentResponse(Long commentId, String content, String name, LocalDateTime createdDate) {
+        this.commentId = commentId;
         this.content = content;
         this.name = name;
         this.createdDate = createdDate;
